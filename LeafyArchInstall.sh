d38ceda9a4d3808c1e7f60563c83094f055896af
Micr0byte <miraslauk@gmail.com>
2022-04-17 14:27:21 -0400

    Add files via upload

--- LeafyArchInstall.sh
+++ LeafyArchInstall.sh
@@ -8,6 +8,32 @@ else
     exit 1
 fi
 
-echo "Cofiguring system clocks..."
+echo -e "\nCofiguring system clocks..."
 timedatectl set-ntp true
-echo "Complete!"
+echo -e "Complete!\n"
+
+while true; do
+    lsblk
+    read -p "Please choose disk for installation: (sda, nvme0n1, etc.)" selectedDisk
+    selectedDisk="/dev/$selectedDisk"
+    read -p "Are you sure you would like to use $selectedDisk? It will be completly wiped and ALL DATA WILL BE LOST (y/n) " diskConfirmation
+    case $diskConfirmation in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo -e "Please answer yes or no.\n";;
+    esac
+done
+
+echo -e "\nDisk $selectedDisk partitions: "
+sfdisk -l
+
+echo -e "\nWiping disk $selectedDisk..." 
+sfdisk --delete $selectedDisk
+echo -e "Complete!"
+
+echo -e "\nDisk $selectedDisk partitions: "
+sfdisk -l
+
+#echo -e "\nCreating /boot..." 
+#sfdisk --delete $selectedDisk
+#echo -e "Complete!"
